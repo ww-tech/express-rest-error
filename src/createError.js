b9aaa4e3e552ac4fa53367fe8c6729f755d47cf6
Alex Oxrud <alex.oxrud@weightwatchers.com>
2020-05-14 16:15:11 -0600

    refactor: cleaned up code and add httpStatus arg

--- src/createError.js
+++ src/createError.js
@@ -1,29 +1,18 @@
-export const customError = (message, details) => {
+export const customError = (message, details, httpStatus) => {
   const err = new Error(message);
   err.details = details;
+  err.httpStatus = httpStatus;
   return err;
 }
 
-export const validationError = (message = 'Validation error.',  details) => {
-  const err = customError(message, details);
-  err.httpStatus = 400;
-  return err;
-}
+export const validationError = (message = 'Validation error.',  details) => 
+  customError(message, details, 400)
 
-export const authRequired = (message = 'Authentication required.', details) => {
-  const err = customError(message, details)
-  err.httpStatus = 401;
-  return err;
-}
+export const authRequired = (message = 'Authentication required.', details) => 
+  customError(message, details, 401)
 
-export const accessDenied = (message = 'Access denied.', details) => {
-  const err = customError(message, details);
-  err.httpStatus = 403;
-  return err;
-}
+export const accessDenied = (message = 'Access denied.', details) => 
+  customError(message, details, 403);
 
-export const notFound = (message = 'Not found.', details) => {
-  const err = customError(message, details);
-  err.httpStatus = 404;
-  return err
-}
+export const notFound = (message = 'Not found.', details) => 
+  customError(message, details, 404);
